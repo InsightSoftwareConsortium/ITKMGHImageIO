fbdf13b36e755f6e5c32a90b51f76a051abbde0d
Mathew Seng <mathewseng@gmail.com>
2020-11-20 08:38:10 -0600

    ENH: Update python package version for modules This will bump the package version in setup.py from 1.0.0 to 1.1.0. In addition, this commit must be tagged and pushed to GitHub.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
 
 setup(
     name='itk-iomgh',
-    version='1.0.0',
+    version='1.1.0',
     author='Insight Software Consortium',
     author_email='community@itk.org',
     packages=['itk'],
